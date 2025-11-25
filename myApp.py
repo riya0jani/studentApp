856f7c8c9bf00767b68be4abc6b1284d9a6f9f58
Your Name <you@example.com>
2025-11-25 11:04:33 +0530

    first commit

---
+++ myApp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return {
+        "status": "SUCCESS",
+        "data":{
+            "regno": 1847207,
+            "name": "ALWIN",
+            "email": "alwin.joseph@mca.christuniverse.in"
+        }
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
